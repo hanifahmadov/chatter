e348868c1151f203cebc602186c68e76327b2d9b
hanif <h.ehmedov@gmail.com>
2024-10-23 18:40:29 -0400

    upload only image files, not video files

--- src/comps/UploadImageButton.js
+++ src/comps/UploadImageButton.js
@@ -1,5 +1,6 @@
 /* npm packages */
 import React, { useRef } from "react";
+import { useRecoilState } from "recoil";
 import { motion } from "framer-motion";
 
 /* images */
@@ -8,17 +9,59 @@ import uploadImages from "../store/image/image-2.svg";
 /* helper */
 import { Fontawesome } from "../store/fontawesome/Fontawesome";
 
+/* defaults */
+import { errorContentDefault, signsErrorDefault } from "../store/states/app_state";
+
+/* Main Functional Component */
 export const UploadImageButton = ({ setImage }) => {
 	/* refs */
 	const imageRef = useRef(null);
+	/* tracking the setTimeout Ids on signin_api error catch */
+	const timeout = useRef(0);
+
+	/* states */
+	const [signsError, setSignsError] = useRecoilState(signsErrorDefault);
+	const [errorContent, setErrorContent] = useRecoilState(errorContentDefault);
 
 	/* upload image */
 	const handleMessageImageChange = () => {
 		const [file] = imageRef.current?.files;
 		if (file) {
-			setImage(file);
-			// Clear the input value to allow the same image to be selected again
-			imageRef.current.value = null;
+			// Get the file extension in lowercase
+			const fileExtension = file.name.split(".").pop().toLowerCase();
+
+			// allowed extensions
+			const allowedExtensions = ["png", "jpeg", "jpg", "avif", "webp"];
+
+			// Check if the file extension is allowed
+			if (!allowedExtensions.includes(fileExtension)) {
+				/* clear the current set-timeouts */
+				clearTimeout(timeout.current);
+
+				/** error toaster
+				 *  content of error
+				 */
+				setErrorContent({
+					text1: "Please select a valid image file",
+					text2: " [ png, jpeg, jpg, avif, webp ] ",
+				});
+
+				/* display error */
+				setSignsError(true);
+
+				/* remove error after 5s automatically */
+				timeout.current = setTimeout(() => {
+					setSignsError(false);
+					setErrorContent({ text1: "", text2: "" });
+					timeout.current = 0;
+				}, 5000);
+			} else {
+				// set the image
+				setImage(file);
+
+				// Clear the input value to allow the same image to be selected again
+				imageRef.current.value = null;
+			}
 		}
 	};
 
@@ -42,7 +85,7 @@ export const UploadImageButton = ({ setImage }) => {
 							
                         '
 			>
-				<span className="text-blue-300">
+				<span className='text-blue-300'>
 					<img src={uploadImages} className='w-[22px] h-[22px]' />
 				</span>
 			</motion.label>
